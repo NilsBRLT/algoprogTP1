5c4fdf951b388e75c918fa91636036f7be98bc50
Pierre-Antoine Porte <porte.pierreantoine@gmail.com>
2015-09-30 18:50:59 +0200

    first commit

---
+++ algoprog_tp1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algoprog_tp1
+//
+//  Created by Pierre-Antoine Porte on 30/09/2015.
+//  Copyright (c) 2015 Porlat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
